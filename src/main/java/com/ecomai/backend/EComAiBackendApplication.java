640943366afb70111b03b6bb904fbccb73dd1a77
ChoiMari <chl98639863@gmail.com>
2026-06-16 20:29:37 +0900

    chore: initialize spring boot project

---
+++ src/main/java/com/ecomai/backend/EComAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecomai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomai/backend/EComAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
